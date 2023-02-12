1f3c935ab67ecfca0015fd0349834671a3432e55
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-02-12 13:27:50 +0400

    ready to present

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,14 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        //TreeSet<Integer> intList = new TreeSet<Integer> (1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        intList.removeIf(x -> x < 0);
+        ArrayList<Integer> intList = new ArrayList<>(List.of(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
+        intList.removeIf(x -> x < 1);
         intList.removeIf(x -> x % 2 != 0);
         intList.sort(Comparator.naturalOrder());
-        System.out.println(intList);
-        //for (Integer integer : intList) {
-        //    System.out.print(integer);
-        //}
-
+        for (Integer i : intList) {
+            System.out.print(i);
+        }
     }
 }
